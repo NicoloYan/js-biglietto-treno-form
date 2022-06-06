3994e5dab26b342308ed07547051c2ea252efcee
NicoloYan <nico.yan@outlook.it>
2022-06-06 18:58:55 +0200

    Proseguo con la funzione

--- js/script.js
+++ js/script.js
@@ -6,32 +6,45 @@
 // Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 const generateTicket = document.getElementById('generate_ticket')
-
-const userName = document.getElementById('user_name').value;
-const userKm = parseInt(document.getElementById('user_km').value);
-const userAge = document.getElementById('user_age').value;
-
-const passengerName = document.getElementById('passenger_name')
-const offer = document.getElementById('offer')
-const wagon = document.getElementById('wagon')
-const cpCode = document.getElementById('CP_code')
-const ticketPrice = document.getElementById('ticket_price')
-
-const priceKm = 0.21
-
-let originalPrice = userKm * priceKm
-let discount = 0
-
-if ( userAge === 'minorenne' ) {
-    discount = originalPrice * 20 / 100
-}
-else if ( userAge === 'over' ) {
-    discount = originalPrice * 40 / 100
-}
-
-let finalPrice = originalPrice - discount
-console.log(finalPrice)
-
 generateTicket.addEventListener('click' , function () {
+let userName = document.getElementById('user_name');
+const userKm = document.getElementById('user_km');
+const userAge = document.getElementById('user_age');
+
+let passengerName = document.getElementById('passenger_name')
+let offer = document.getElementById('offer')
+let wagon = document.getElementById('wagon')
+let cpCode = document.getElementById('CP_code')
+let discount = 0
+console.log(discount)
+
+
+
+    const priceKm = 0.21
+    passengerName.innerHTML = userName.value;
+    wagon.innerHTML = Math.floor(Math.random() * 10);
+    cpCode.innerHTML = Math.floor(Math.random() * 10000);
+    let originalPrice = userKm.value * priceKm;
+    console.log(originalPrice)
+    let finalPrice = originalPrice - discount;
+    let ticketPrice = document.getElementById('ticket_price')
+    console.log(ticketPrice.value)
+
     
+    if ( userAge.value == 'minorenne' ) {
+        discount = parseInt(originalPrice * 20 / 100);
+        
+        offer.innerHTML = 'Tariffa minorenne';
+        finalPrice = originalPrice - discount;
+        ticketPrice.innerHTML = finalPrice;
+    }
+    else if ( userAge.value == 'over' ) {
+        discount = parseInt(originalPrice * 40 / 100);
+        
+        offer.innerHTML = 'Tariffa Over 65';
+        finalPrice = originalPrice - discount;
+        ticketPrice.innerHTML = finalPrice;
+    } 
+
+    userName.value = ''
 })
